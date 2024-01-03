fd80e6d7508fe7efba5340e6ae82aff04b35e662
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2024-01-04 00:24:06 +0200

    add logic of sorting

---
+++ src/mock-data.ts
@@ -0,0 +1,98 @@
+export const data: object[] = [
+   {
+      width: 43,
+      height: 29,
+      name: "name 1",
+      id: "1",
+   },
+   {
+      width: 6,
+      height: 40,
+      name: "name 2",
+      id: "2",
+   },
+   {
+      width: 81,
+      height: 4,
+      name: "name 3",
+      id: "3",
+   },
+   {
+      width: 23,
+      height: 82,
+      name: "name 4",
+      id: "4",
+   },
+   {
+      width: 28,
+      height: 10,
+      name: "name 5",
+      id: "5",
+   },
+   {
+      width: 29,
+      height: 3,
+      name: "name 6",
+      id: "6",
+   },
+   {
+      width: 29,
+      height: 42,
+      name: "name 7",
+      id: "7",
+   },
+   {
+      width: 54,
+      height: 79,
+      name: "name 8",
+      id: "8",
+   },
+   {
+      width: 33,
+      height: 68,
+      name: "name 9",
+      id: "9",
+   },
+   {
+      width: 12,
+      height: 87,
+      name: "name 10",
+      id: "10",
+   },
+   {
+      width: 82,
+      height: 33,
+      name: "name 11",
+      id: "11",
+   },
+   {
+      width: 75,
+      height: 44,
+      name: "name 12",
+      id: "12",
+   },
+   {
+      width: 20,
+      height: 82,
+      name: "name 13",
+      id: "13",
+   },
+   {
+      width: 98,
+      height: 33,
+      name: "name 14",
+      id: "14",
+   },
+   {
+      width: 19,
+      height: 86,
+      name: "name 15",
+      id: "15",
+   },
+   {
+      width: 48,
+      height: 64,
+      name: "name 16",
+      id: "16",
+   },
+];
